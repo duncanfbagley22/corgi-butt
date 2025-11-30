0b99ccc2edfb7010bf00d50492a1cc24d1e16746
Duncan Bagley <duncanfbagley22@gmail.com>
2025-11-30 16:52:04 -0500

    updates for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   images: {
     unoptimized: isProd, // Only unoptimize for production
-  },
-  basePath: isProd ? '/corgi-butt' : '', // Only use basePath in production
-  assetPrefix: isProd ? '/corgi-butt/' : '', // Only use assetPrefix in production
+  }
 };
 
 export default nextConfig;
